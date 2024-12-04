7614989e9a6bccf474050a1d67af37c7c31e0a2a
Genaro Gonzalez <genarrogg@gmail.com>
2024-12-04 15:39:20 -0400

    btns

--- src/components/Index.tsx
+++ src/components/Index.tsx
@@ -1,6 +1,24 @@
 import Gravatar from "./gravatar/Gravatar";
 import { A, Icon, notify } from "./nano/Index";
 
-import { BtnLoki } from "./btns/index"
+import {
+    BtnLoki,
+    BtnFreya,
+    BtnThor,
 
-export { Gravatar, A, Icon, notify, BtnLoki };
+    BtnSubmitBasic,
+    BtnNormalBasic,
+    BtnText,
+} from "./btns/index"
+
+export {
+    Gravatar, A, Icon, notify,
+    BtnLoki,
+    BtnFreya,
+    BtnThor,
+
+
+    BtnSubmitBasic,
+    BtnNormalBasic,
+    BtnText,
+};
--- src/components/btns/index.tsx
+++ src/components/btns/index.tsx
@@ -1,8 +1,19 @@
-
+/* btn hamburguesa */
 import BtnLoki from "./hamburguesa/BtnLoki";
 import BtnFreya from "./hamburguesa/BtnFreya";
+import BtnThor from "./hamburguesa/BtnThor";
+
+/* btn basic */
+import BtnSubmitBasic from "./basic/BtnSubmitBasic";
+import BtnNormalBasic from "./basic/BtnNormalBasic";
+import BtnText from "./basic/BtnText";
 
 export {
   BtnLoki,
-  BtnFreya
+  BtnFreya,
+  BtnThor,
+
+  BtnSubmitBasic,
+  BtnNormalBasic,
+  BtnText,
 };
