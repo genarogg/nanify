3829b82a1fe15b5f359a56b036fac35b91a5344b
Genaro Gonzalez <genarrogg@gmail.com>
2024-12-09 08:05:47 -0400

    BIFURCACION

--- src/pages/_app.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "../sass/nanify.scss";
-
-import type { AppProps } from "next/app";
-
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
