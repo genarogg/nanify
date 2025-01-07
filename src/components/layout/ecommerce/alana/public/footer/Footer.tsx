675a70115e2cc66d5c614f34514bbe9ad44f8896
Genaro Gonzalez <genarrogg@gmail.com>
2025-01-07 01:28:04 -0400

    mor

--- src/components/layout/ecommerce/alana/public/footer/Footer.tsx
+++ src/components/layout/ecommerce/alana/public/footer/Footer.tsx
@@ -1,27 +1,52 @@
-import React from 'react'
-import './sass/_footer.scss'
+import React from 'react';
+import './sass/_footer.scss';
 import { LogoGenarogg, A, Icon } from 'nanify';
 import { FaHourglassEnd } from "react-icons/fa6";
 
-interface FooterProps {
-
-}
+interface FooterProps { }
 
 const Footer: React.FC<FooterProps> = () => {
     const currentYear = new Date().getFullYear();
+
+    const footerData = {
+        logo: 'alana',
+        address: '1234 Fashion Street, Suite 567, New York, NY 10001',
+        email: 'info@fashionshop.com',
+        phone: '(212) 555-1234',
+        helpLinks: [
+            { href: '/', text: 'Order Status' },
+            { href: '/', text: 'Returns + Exchanges' },
+            { href: '/', text: 'Shipping' },
+            { href: '/', text: 'Terms + Conditions' },
+            { href: '/', text: 'FAQ’s' },
+            { href: '/', text: 'Compare' },
+            { href: '/', text: 'My Wishlist' },
+        ],
+        aboutLinks: [
+            { href: '/', text: 'About Us' },
+            { href: '/', text: 'Our Story' },
+            { href: '/', text: 'Careers' },
+            { href: '/', text: 'Press' },
+        ],
+        socialLinks: [
+            { href: 'A', icon: <FaHourglassEnd /> },
+            { href: '#', icon: <FaHourglassEnd /> },
+            { href: '#', icon: <FaHourglassEnd /> },
+            { href: '#', icon: <FaHourglassEnd /> },
+        ],
+    };
+
     return (
         <footer className='footer'>
             <div className="footerDesktop">
                 <ul className='containerUl'>
                     <li className='col-1'>
                         <h6 className="logo">
-                            <A href='/'>
-                                alana
-                            </A>
+                            <A href='/'>{footerData.logo}</A>
                         </h6>
                         <ul>
                             <li>
-                                <label htmlFor="">
+                                <label>
                                     lorem ipsum dolor sit amet, consectetur adipisicing elit.
                                     lorem ipsum dolor sit amet, consectetur adipisicing elit.
                                 </label>
@@ -30,21 +55,21 @@ const Footer: React.FC<FooterProps> = () => {
                                 <label>
                                     <Icon icon={<FaHourglassEnd />} />
                                     <span>Address:</span>
-                                    <strong>1234 Fashion Street, Suite 567,New York, NY 10001</strong>
+                                    <strong>{footerData.address}</strong>
                                 </label>
                             </li>
                             <li>
                                 <label>
                                     <Icon icon={<FaHourglassEnd />} />
                                     <span>Email:</span>
-                                    <strong>info@fashionshop.com</strong>
+                                    <strong>{footerData.email}</strong>
                                 </label>
                             </li>
                             <li>
                                 <label>
                                     <Icon icon={<FaHourglassEnd />} />
                                     <span>Phone:</span>
-                                    <strong>(212) 555-1234</strong>
+                                    <strong>{footerData.phone}</strong>
                                 </label>
                             </li>
                         </ul>
@@ -52,108 +77,45 @@ const Footer: React.FC<FooterProps> = () => {
                     <li className='col-2'>
                         <h6>Help</h6>
                         <ul>
-                            <li>
-                                <A href='/'>
-                                    <Icon icon={<FaHourglassEnd />} />
-                                    <span>Order Status</span>
-                                </A>
-                            </li>
-                            <li>
-                                <A href='/'>
-                                    <Icon icon={<FaHourglassEnd />} />
-                                    <span>Returns + Exchanges</span>
-                                </A>
-                            </li>
-                            <li>
-                                <A href='/'>
-                                    <Icon icon={<FaHourglassEnd />} />
-                                    <span>Shipping</span>
-                                </A>
-                            </li>
-                            <li>
-                                <A href='/'>
-                                    <Icon icon={<FaHourglassEnd />} />
-                                    <span>Terms + Conditions</span>
-                                </A>
-                            </li>
-                            <li>
-                                <A href="/">
-                                    <Icon icon={<FaHourglassEnd />} />
-                                    <span>FAQ’s</span>
-                                </A>
-                            </li>
-                            <li>
-                                <A href="/">
-                                    <Icon icon={<FaHourglassEnd />} />
-                                    <span>Compare</span></A>
-                            </li>
-                            <li>
-                                <A href="/">
-                                    <Icon icon={<FaHourglassEnd />} />
-                                    <span>My Wishlist</span>
-                                </A>
-                            </li>
-
+                            {footerData.helpLinks.map((link, index) => (
+                                <li key={index}>
+                                    <A href={link.href}>
+                                        <Icon icon={<FaHourglassEnd />} />
+                                        <span>{link.text}</span>
+                                    </A>
+                                </li>
+                            ))}
                         </ul>
                     </li>
                     <li className='col-3'>
                         <h6>About us</h6>
                         <ul>
-                            <li>
-                                <A href="/">
-                                    <Icon icon={<FaHourglassEnd />} />
-                                    <span>My Wishlist</span>
-                                </A>
-                            </li>
-                            <li>
-                                <A href="/">
-                                    <Icon icon={<FaHourglassEnd />} />
-                                    <span>My Wishlist</span>
-                                </A>
-                            </li>
-                            <li>
-                                <A href="/">
-                                    <Icon icon={<FaHourglassEnd />} />
-                                    <span>My Wishlist</span>
-                                </A>
-                            </li>
-                            <li>
-                                <A href="/">
-                                    <Icon icon={<FaHourglassEnd />} />
-                                    <span>My Wishlist</span>
-                                </A>
-                            </li>
+                            {footerData.aboutLinks.map((link, index) => (
+                                <li key={index}>
+                                    <A href={link.href}>
+                                        <Icon icon={<FaHourglassEnd />} />
+                                        <span>{link.text}</span>
+                                    </A>
+                                </li>
+                            ))}
                         </ul>
                     </li>
                 </ul>
             </div>
             <div className="footerDown">
                 <div className="left">
-                <p>© {currentYear} alana. Todos los derechos reservados.</p>
+                    <p>© {currentYear} {footerData.logo}. Todos los derechos reservados.</p>
                 </div>
                 <div className="right">
                     <div className="redes">
                         <ul>
-                            <li>
-                                <A type="a" href="A">
-                                    <Icon icon={<FaHourglassEnd />} />
-                                </A>
-                            </li>
-                            <li>
-                                <A type="a" href="#">
-                                    <Icon icon={<FaHourglassEnd />} />
-                                </A>
-                            </li>
-                            <li>
-                                <A type="a" href="#">
-                                    <Icon icon={<FaHourglassEnd />} />
-                                </A>
-                            </li>
-                            <li>
-                                <A type="a" href="#">
-                                    <Icon icon={<FaHourglassEnd />} />
-                                </A>
-                            </li>
+                            {footerData.socialLinks.map((link, index) => (
+                                <li key={index}>
+                                    <A type="a" href={link.href}>
+                                        <Icon icon={link.icon} />
+                                    </A>
+                                </li>
+                            ))}
                         </ul>
                     </div>
                 </div>
