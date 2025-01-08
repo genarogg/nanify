f7fe908a5f3ac7cdad583d570814026c4224053e
Genaro Gonzalez <genarrogg@gmail.com>
2025-01-08 19:17:28 -0400

    imgs

--- src/components/cards/ecomerce/CardProductoAlana.tsx
+++ src/components/cards/ecomerce/CardProductoAlana.tsx
@@ -20,29 +20,12 @@ const CardProductoAlana: React.FC<CardProductoAlanaProps> = () => {
         titulo: "Producto 1",
         colores: [{
             color: "#000",
-            mainImg: "https://themesflat.co/html/ecomus/images/products/white-4.jpg",
-            imgs: [
-                {
-                    src: "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg",
-                    alt: "imagen 1"
-                },
-                {
-                    src: "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg",
-                    alt: "imagen 2"
-                },
-                {
-                    src: "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg",
-                    alt: "imagen 2"
-                }
-            ],
+            primaryImg: "https://themesflat.co/html/ecomus/images/products/white-4.jpg",
+            secundaryImg: "https://themesflat.co/html/ecomus/images/products/pink-1.jpg",
             precio: 100,
             tallas: ["S", "M", "L", "XL"],
         }],
-       
-        
-       
-       
-        
+
     }
 
     const quickActions = [
@@ -54,51 +37,49 @@ const CardProductoAlana: React.FC<CardProductoAlanaProps> = () => {
 
     return (
         <div className="card-alana-producto">
-            <div className="container-img">
-                <A href={data.url} className='main-img'>
-                    <Img
-                        type='remote'
-                        src={data.mainImg}
-                        alt={data.titulo}
-                        width={360}
-                        height={460}
-                    />
-                </A>
-                <nav className='quik-actions'>
-                    <ul>
-                        {quickActions.map((action, index) => (
-                            <li key={index}>
-                                <MenuToolTip
-                                    interactive={false}
-                                    items={action.items}
-                                    placement='top'
-                                    classNameTooltip='card-producto-alana-quik-actions-tooltip'
-                                >
-                                    <Icon icon={action.icon} />
-                                </MenuToolTip>
-                            </li>
-                        ))}
-                    </ul>
-                </nav>
-                <nav className='tallas'>
-                    <ul>
-                        {
-                            data.tallas.map((talla, index) => (
+            {data.colores.map((color, colorIndex) => (
+                <div key={colorIndex} className="container-img">
+                    <A href={data.url} className="main-img">
+                        <Img
+                            type="remote"
+                            src={color.primaryImg}
+                            alt={data.titulo}
+                            width={360}
+                            height={460}
+                        />
+                    </A>
+                    <nav className="quik-actions">
+                        <ul>
+                            {quickActions.map((action, index) => (
                                 <li key={index}>
-                                    <A href={`${data.url}#talla=${talla}`} className='talla'>
+                                    <MenuToolTip
+                                        interactive={false}
+                                        items={action.items}
+                                        placement="top"
+                                        classNameTooltip="card-producto-alana-quik-actions-tooltip"
+                                    >
+                                        <Icon icon={action.icon} />
+                                    </MenuToolTip>
+                                </li>
+                            ))}
+                        </ul>
+                    </nav>
+                    <nav className="tallas">
+                        <ul>
+                            {color.tallas.map((talla, index) => (
+                                <li key={index}>
+                                    <A href={`${data.url}#talla=${talla}`} className="talla">
                                         {talla}
                                     </A>
                                 </li>
-                            ))
-                        }
-                    </ul>
-                </nav>
-
-
-            </div>
+                            ))}
+                        </ul>
+                    </nav>
+                </div>
+            ))}
             <div className="container-info">
                 <h3>{data.titulo}</h3>
-                <p>${data.precio}</p>
+                <p>${data.colores[0]?.precio}</p>
             </div>
         </div>
     );
