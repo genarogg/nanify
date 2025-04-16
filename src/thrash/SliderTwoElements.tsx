555817bc0b96c3f477227899d3154124c0747df1
Genaro Gonzalez <gena_gonzalez15@hotmail.com>
2025-04-17 01:14:08 +0800

    up

--- src/thrash/SliderTwoElements.tsx
+++ src/thrash/SliderTwoElements.tsx
@@ -1,44 +1,44 @@
-"use client";
-import React from 'react';
-import "./sliderBackgroud/sass/_sliderBackgroud.scss";
-import { SwiperSlide } from 'swiper/react';
+// "use client";
+// import React from 'react';
+// import "./sliderBackgroud/sass/_sliderBackgroud.scss";
+// import { SwiperSlide } from 'swiper/react';
 
-import SwiperLGTwoElements from '@slider/swiper/estructura/SwiperLGTwoElements';
+// import SwiperLGTwoElements from '@slider/swiper/estructura/SwiperLGTwoElements';
 
-import img1 from "@public/swiper/people-1.jpg";
-import img2 from "@public/swiper/people-2.jpg";
-
-
-interface SliderBackgroundProps { }
-
-const SliderBackground: React.FC<SliderBackgroundProps> = () => {
-
-    const elements = [
-        {
-            img: img1
-        },
-        {
-            img: img2
-        },
+// import img1 from "@public/swiper/people-1.jpg";
+// import img2 from "@public/swiper/people-2.jpg";
 
 
-    ];
+// interface SliderBackgroundProps { }
 
-    return (
-        <div className='containerSliderLg bg'>
-            <SwiperLGTwoElements >
-                {elements.map((element, index) => (
-                    <SwiperSlide key={index}>
-                        <img
-                            src={element.img.src}
-                            alt="img"
-                            className="swiper-gl-image"
-                        />
-                    </SwiperSlide>
-                ))}
-            </SwiperLGTwoElements>
-        </div>
-    );
-}
+// const SliderBackground: React.FC<SliderBackgroundProps> = () => {
 
-export default SliderBackground
+//     const elements = [
+//         {
+//             img: img1
+//         },
+//         {
+//             img: img2
+//         },
+
+
+//     ];
+
+//     return (
+//         <div className='containerSliderLg bg'>
+//             <SwiperLGTwoElements >
+//                 {elements.map((element, index) => (
+//                     <SwiperSlide key={index}>
+//                         <img
+//                             src={element.img.src}
+//                             alt="img"
+//                             className="swiper-gl-image"
+//                         />
+//                     </SwiperSlide>
+//                 ))}
+//             </SwiperLGTwoElements>
+//         </div>
+//     );
+// }
+
+// export default SliderBackground
